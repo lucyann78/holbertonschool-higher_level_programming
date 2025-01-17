4dbefad8391ddcfd2d8ec2f3a7903e0ee52b6fbb
lucyann <lucyann78@gmail.com>
2025-01-17 15:25:40 -0400

    create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67]  # Extracting "object-oriented programming"
+str = str[39:67] + " with Python"  # Extracting and appending " with Python"
 print(str)
