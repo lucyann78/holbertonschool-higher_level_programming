fa5def8380ded478764eaa68bdfb8df00866de77
lucyann <lucyann78@gmail.com>
2025-01-18 17:20:40 -0400

    Print all the names

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,21 @@
-#!/usr/bin/python3
+import pkgutil
+import types
 
+def print_hidden_names():
+    # Load the compiled module
+    module_name = 'hidden_4'
+    module = __import__(module_name)
+
+    # Get all names defined in the module
+    names = dir(module)
+
+    # Filter names that do not start with '__' and sort them
+    filtered_names = sorted(name for name in names if not name.startswith('__'))
+
+    # Print each name on a new line
+    for name in filtered_names:
+        print(name)
+
+# This code will not run if the module is imported
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    print_hidden_names()
