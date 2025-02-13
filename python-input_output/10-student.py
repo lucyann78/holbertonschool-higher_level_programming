79df37253b1834ba9a798e32240a5781801dd5bc
lucyann <lucyann78@gmail.com>
2025-02-13 19:36:40 -0400

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-    class Student that defines a student by:
+    Class Student that defines a student by:
         (based on 9-student.py)
 
     Public instance attributes:
         first_name
         last_name
         age
+
     Instantiation with first_name, last_name and age:
     def __init__(self, first_name, last_name, age):
+
     Public method def to_json(self, attrs=None): that
     retrieves a dictionary representation of a Student instance
     (same as 10-class_to_json.py):
         If attrs is a list of strings, only attribute
         names contained in this list must be retrieved.
-        Otherwise, all attributes must be retrieved
-    You are not allowed to import any module
+        Otherwise, all attributes must be retrieved.
+    You are not allowed to import any module.
 """
 
 
 class Student:
-    """ class to create a dict obj """
+    """ Class to create a dict object """
 
     def __init__(self, first_name, last_name, age):
-        """ initialization for Student object """
-        self.age = age
-        self.last_name = last_name
+        """ Initialization for Student object """
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
-        """ retrieves a dictionary representation """
+        """ Retrieves a dictionary representation """
         dic = {}
-        if type(attrs) != list:
+        if attrs is None or not isinstance(attrs, list):
             return self.__dict__
         else:
             for item in attrs:
-                if type(item) != str:
+                if not isinstance(item, str):
                     return self.__dict__
             for key in self.__dict__:
                 if key in attrs:
