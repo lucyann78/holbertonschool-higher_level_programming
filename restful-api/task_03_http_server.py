85e842334d7bcf43ebb0c7a256556caaa40d2cb3
lucyann <lucyann78@gmail.com>
2025-02-22 16:50:57 -0400

    Develop a simple API using Python with the `http.server` module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,8 +4,8 @@ import http.server
 import socketserver
 import json
 
-
 PORT = 8000
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
@@ -24,21 +24,17 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 "city": "New York"
             }
             self.wfile.write(json.dumps(response_data).encode('utf-8'))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "status": "OK"
-            }"""
             self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "error": "Endpoint not found"
-            }"""
             self.wfile.write(b"404 Not Found")
 
 Handler = MyHandler
