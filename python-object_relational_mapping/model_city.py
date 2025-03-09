b4a41b840895280b0a7f3ade57c3017bfeb5d544
lucyann <lucyann78@gmail.com>
2025-03-09 11:12:30 -0400

    Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class that represents the `cities` table"""
     __tablename__ = 'cities'
